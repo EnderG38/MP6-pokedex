eae46e4ec41f8cad06ac385427e49e390e3908cb
Gus Soares <gsoares@illinois.edu>
2018-12-02 21:22:04 -0600

    about page works, all that's needed is to give it the proper text

--- app/src/main/java/mp/dex/AboutActivity.java
+++ app/src/main/java/mp/dex/AboutActivity.java
@@ -3,7 +3,7 @@ package mp.dex;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
