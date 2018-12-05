128ee5d65756d5f9140dc34952ba9d47bc8f3bce
Gus Soares <gsoares@illinois.edu>
2018-12-04 22:13:35 -0600

    implemented sprite loading on app load. please use conservatively as cache is not used. Text loading does not work yet.

--- app/src/main/java/mp/dex/Contenthandler.java
+++ app/src/main/java/mp/dex/Contenthandler.java
@@ -1,46 +1,39 @@
 package mp.dex;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.android.volley.toolbox.StringRequest;
 import com.squareup.picasso.Picasso;
 import com.android.volley.*;
-import com.android.volley.toolbox.ImageRequest;
 
 //this class handles the json grabbing and parsing.
 public class Contenthandler extends AppCompatActivity {
-    private static String urlAppend;
-    private static final String URL_BASE = "https://pokeapi.co/api/v2/";
-    private static final String URL_SPRITE_BASE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
-    private static RequestQueue requestQueue;
-    protected static Bitmap bitmap = null;
+    protected static final String URL_BASE = "https://pokeapi.co/api/v2/";
+    protected static final String URL_SPRITE_BASE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-    //TODO: remove Volley from everywhere
-    public Bitmap getSpritesTest(final ImageView imageView) {
-        final String url = URL_SPRITE_BASE + "1.png";
-        final ImageRequest imageRequest = new ImageRequest(url,
-                new Response.Listener<Bitmap>() {
-                    @Override
-                    public void onResponse(Bitmap response) {
-                        imageView.setImageBitmap(response);
-                        bitmap = response;
-                        Log.i("tag", "it shoulda worked");
-                    }
-                },
-                50, 50, null,
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("tag", "shit happend m89");
-                    }
-                });
-        Log.i("tag", "test");
-        return bitmap;
+    public static void getSpriteTest(final ImageView imageView) {
+        Picasso.get().load(URL_SPRITE_BASE + "1.png").resize(300, 300).into(imageView);
     }
 
-    public static void getSpriteTest2(final ImageView imageView1) {
-        Picasso.get().load(URL_SPRITE_BASE + "1.png").resize(300, 300).into(imageView1);
+    public static TextView getPokemon(final int id) {
+        final TextView ret = new TextView(new Contenthandler().getApplicationContext());
+        String url = URL_BASE + id;
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        ret.setText(response.substring(response.indexOf("\"forms\"") + 1, response.indexOf("\"", response.indexOf("\"forms\"") + 30)));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                ret.setText("Error! id: " + id);
+            }
+        });
+        return ret;
     }
 }
